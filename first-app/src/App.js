18a01ca3d1ccd89b5dfde95728fa01041731770f
Luam Leite <luamleite@nasajon.com.br>
2021-02-16 01:44:10 -0300

    state, setState and refactoring to Class app

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,31 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
+class App extends Component { 
   //Only use state to manage internal component data, you don't want to use globally and not much
-  React.state = {
-    persons: [
-      { name: 'Max', age: 28},
-      { name: 'Manu', age: 29},
-      { name: 'Stephanie', age: 26}
-    ],
-    switchNameHandler: () => {
-      console.log('Was clicked!');
-    }
+ state = {
+   persons: [
+     { name: 'Max', age: 28 },
+     { name: 'Manu', age: 29 },
+     { name: 'Stephanie', age: 26 }
+   ],
+   otherState: 'some other value'
+ } //We just want to be executed when clicked
+
+  switchNameHandler = () => {
+    console.log('Was clicked');
+    this.setState({
+      persons: [
+        { name: 'Maximillian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
   }
 
-  //good pratice to name as Handler
-  //We don't add parenthesis to the onClick, because if we do, the function will be instantly executed as the dom loads.
-  //We just want to be executed when clicked
-
+render(){
   return (
     <div className="App">
     <h1> Hi, I am React</h1>
-    <button onClick={React.state.switchNameHandler}> Switch Name </button>
-    <Person name={React.state.persons[0].name} age={React.state.persons[0].age}/>
-    <Person name={React.state.persons[1].name} age ={React.state.persons[1].age}/>
-    <Person name={React.state.persons[2].name} age={React.state.persons[0].age}> My Hobbies: Racing </Person>
+    <button onClick={this.switchNameHandler}> Switch Name </button>
+    <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+    <Person name={this.state.persons[1].name} age ={this.state.persons[1].age}/>
+    <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> My Hobbies: Racing </Person>
     </div>
   );
+  }
 }
 export default App;
